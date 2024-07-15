feee62a2a4e06ad6bf5ae352caf5da9030016b43
joelam21 <joelam21@gmail.com>
2024-07-15 13:49:14 -0700

    query for order_items to add category name and customer_id

---
+++ order_items_products.sql
@@ -0,0 +1,13 @@
+WITH product_translated AS (
+    SELECT p.product_id, t.product_category_name_english category 
+    FROM products p
+    LEFT JOIN cat_translation t 
+    ON p.product_category_name = t.product_category_name
+)
+
+SELECT i.*, p.category, o.customer_id
+FROM order_items i
+LEFT JOIN product_translated p
+ON i.product_id = p.product_id
+LEFT JOIN orders o
+ON o.order_id = i.order_id
